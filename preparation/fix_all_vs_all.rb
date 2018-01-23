707f8da086a3574010866ff26966cc6e3e57a1d9
Serafim Nenarokov <serafim221@gmail.com>
2018-01-23 01:59:34 +0100

    Adds all vs all files preparation

---
+++ preparation/fix_all_vs_all.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'slop'
+require 'pathname'
+require 'fileutils'
+require 'ruby-progressbar'
+require 'pry'
+
+params = Slop.parse do |o|
+  o.banner = "Prepare MMETSP all vs all hits for Decross"
+  o.string '--wrong_names_path', 'wrong names .csv file'
+  o.string '--all_vs_all_path', 'folder with .blastab files'
+  o.on '-h', '--help', 'Print options' do
+    puts o
+    exit
+  end
+end
+
+
+names = File.open(params[:wrong_names_path]).readlines.map{ |l| l.strip.split(',') }
+pb = ProgressBar.create(title: 'Fixing blastabs', starting_at: 0, total: names.count)
+
+names.each do |n|
+  new_name = n[0]
+  old_name = n[1]
+
+  Dir["#{params[:all_vs_all_path]}/*#{new_name}*.blastab"].each do |path|
+    `pru 'gsub("#{old_name}", "#{new_name}")' -i #{path}`
+  end
+
+  pb.increment
+end
