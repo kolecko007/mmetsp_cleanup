4ae5e0bae08dea402f7d64df88d026205a869c04
Serafim Nenarokov <serafim221@gmail.com>
2018-01-25 22:08:36 +0100

    Add --reverse option support

--- preparation/fix_one_vs_all.rb
+++ preparation/fix_one_vs_all.rb
@@ -8,11 +8,12 @@ require 'pry'
 require 'parallel'
 
 params = Slop.parse do |o|
-  o.banner = "Prepare MMETSP one vs all hits for Decross"
+  o.banner = "Prepare MMETSP one vs all hits for Decross. Files are sorted before the sed."
   o.string '--wrong_names_path', 'wrong names .csv file'
   o.string '--one_vs_all_path', 'folder with .fas files'
   o.integer '--threads', 'Number of threads for the proccess'
   o.integer '--files_limit', 'Number of files to proccess (for testing)'
+  o.boolean '--reverse', 'Begin from the last file to the first'
   o.on '-h', '--help', 'Print options' do
     puts o
     exit
@@ -25,6 +26,7 @@ pru_command = ENV['PRU_EXEC'] || 'pru'
 pru_options = "#{names.map { |n| "gsub(\"#{n[1]}\", \"#{n[0]}\")" }.join('.')}"
 
 paths = Dir["#{params[:one_vs_all_path]}/*.blastab"].sort
+paths.reverse! if params[:reverse]
 paths = paths.first(params[:files_limit]) if params[:files_limit]
 
 thread_num = params[:threads] || 1
