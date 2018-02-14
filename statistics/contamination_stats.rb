117a9d9f8e0e75ccba21437c0066c5c6a4cfb06b
Serafim Nenarokov <serafim221@gmail.com>
2018-02-14 17:02:57 +0100

    Add dataset seq count to statistics

--- statistics/contamination_stats.rb
+++ statistics/contamination_stats.rb
@@ -6,6 +6,7 @@ require 'ruby-progressbar'
 
 params = Slop.parse do |o|
   o.banner = "Prepare MMETSP datasets for Decross"
+  o.string '--datasets_path', '(required) Folder with datasets .fas files'
   o.string '--results_path', '(required) Path of Decross the result folder'
   o.string '--output', '(required) Path for the output file'
   o.on '-h', '--help', 'Print options' do
@@ -15,7 +16,12 @@ params = Slop.parse do |o|
 end
 
 results_path = File.join(params[:results_path], '')
+datasets_path = File.join(params[:datasets_path], '')
 output_path = File.join(params[:output])
+
+def count_dataset_contigs(org_id)
+  `grep -c '>' #{datasets_path}#{org_id}.fas`
+end
 
 files = Dir["#{results_path}*_deleted_stats.csv"]
 result = []
@@ -23,7 +29,6 @@ pb = ProgressBar.create(title: 'Working with files', starting_at: 0, total: file
 
 files.each do |path|
   org_id = path[/MMETSP\d{4}/]
-
 
   clean_path = "#{results_path}#{org_id}_clean.fasta"
   deleted_path = "#{results_path}#{org_id}_deleted.fasta"
@@ -42,12 +47,14 @@ files.each do |path|
     end
   end
 
+  contigs_cnt = count_dataset_contigs(org_id)
+
   pb.increment
-  result << [org_id, clean_cnt, contaminations_cnt, food_cnt, other_cnt].join(',')
+  result << [org_id, contigs_cnt, clean_cnt, contaminations_cnt, food_cnt, other_cnt].join(',')
 end
 
 File.open(output_path, 'w') do |f|
-  f.puts('mmetsp_id,clean,food,other')
+  f.puts('mmetsp_id,contigs,clean,dirty,food,other')
   result.each { |e| f.puts(e) }
 end
 
